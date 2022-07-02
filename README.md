ac30527dc740fb324eca8aac4052ae62dbc1e601
Harry Stevens <hstevens9709@gmail.com>
2022-07-02 18:34:29 +0100

    Undo last commit. Represent server via a ball icon. Score is now p1:p2 again.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 App icon: <a target="_blank" href="https://icons8.com/icon/9791/tennis-racquet">Tennis Racquet</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+
+Tennis ball icon: <a target="_blank" href="https://icons8.com/icon/pUIaQzxWFbcl/tennis-ball">Tennis Ball</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
--- app.js
+++ app.js
@@ -28,14 +28,26 @@ function drawStartScreen() {
 function drawScoreScreen() { 
   var layout = new Layout( {
     type:"v", c: [
-      {type:"txt", font:"20%", label:p1GamesWon.toString() + "-" + p2GamesWon.toString(), id:"setScore", pad:"20"},
       {type:"h", c: [
-        {type:"btn", font:"25%", label:serverScore, cb: l=>incScore(layout.server, layout.receiver), id:"server", pad:"10"},
-        {type:"btn", font:"25%", label:receiverScore, cb: l=>incScore(layout.receiver, layout.server), id:"receiver", pad:"10"}
+        {type:"img", src:maybeDrawBall(firstServer), fillx:1},
+        {type:"txt", font:"20%", label:p1GamesWon.toString() + "-" + p2GamesWon.toString(), id:"setScore", pad:"20"},
+        {type:"img", src:maybeDrawBall((firstServer + 1) % 2), fillx:1}
+    ]},
+      {type:"h", c: [
+        {type:"btn", font:"25%", label:serverScore, cb: l=>incScore(layout.p1, layout.p2), id:"p1", pad:"10"},
+        {type:"btn", font:"25%", label:receiverScore, cb: l=>incScore(layout.p2, layout.p1), id:"p2", pad:"10"}
       ], filly:"1"}
     ]
   });
   draw(layout);
+}
+
+function maybeDrawBall(shouldDrawBall) {
+  if (shouldDrawBall == 0) {
+    return require("heatshrink").decompress(atob("mEwwhC/AFcCkAVTn////w//zCp8DCoIXDAAIzNgQWDC4n/Ih4QBn/wAwZKLh4OBmH/CgPwl41EC5IMCMAfzD4RJLBwPzMAoXFGBBBBmBZDAAIdFMI4NBIAxkCJBYUBRAhBDJBgUBH4oACbwn/+SNGIw5IOCg4pDVJQ7FAA0gGwrsGABPzEoh4EC5gqBBwYXSJIIxCC4kvC5oYBgEjL4gXPMYIZBC6jYIeAIXTh8yYRqPKQYYXTWoJkNC4grEgDcNOwghEbpiOFBQoXRLYgwLXwwRFSRR2EJA8wIx7vG+CqI+YXHJAnzMBBGHJAxgICxAwFkAuQMIvwLpxJHC40gC5kCC4SWECxpKDC4ZENGY4rQAH4Aa"));
+  } else {
+    return require("heatshrink").decompress(atob("mEwghC/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A="));
+  }
 }
 
 function startGame(firstServerInput) {
@@ -74,7 +86,7 @@ function incScore(scorer, otherPlayer) {
   // If the game is still going just update the score,
   // else call a function to reset the game score and update the set score
   if (!gameEnd) {
-    if (scorer.id == "server") {
+    if (scorer.id == "p1") {
         serverScore = newScore; 
         receiverScore = otherPlayerScore;
     } else {
@@ -88,12 +100,10 @@ function incScore(scorer, otherPlayer) {
 }
 
 function endGame(winner) {
-  if (winner.id == "server") {
-    p1GamesWon += ((1 + firstServer) % 2);
-    p2GamesWon += firstServer;
+  if (winner.id == "p1") {
+    p1GamesWon += 1;
   } else {
-    p2GamesWon += ((1 + firstServer) % 2);
-    p1GamesWon += firstServer;
+    p2GamesWon += 1;
   }
   if (isSetWon()) {
     Terminal.println("Set won!");
