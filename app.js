e1d3e9eb26e79c8965aa3ea5dd480126de678db8
Harry Stevens <hstevens9709@gmail.com>
2022-07-02 18:04:14 +0100

    Change game score so it's server:receiver rather than p1:p2

--- app.js
+++ app.js
@@ -1,20 +1,52 @@
 var p1GamesWon = 0;
 var p2GamesWon = 0;
 
+var serverScore = "0";
+var receiverScore = "0";
+
+var firstServer;
+
 var Layout = require("Layout");
-var layout = new Layout( {
-  type:"v", c: [
-    {type:"txt", font:"20%", label:p1GamesWon.toString() + "-" + p2GamesWon.toString(), id:"setScore", pad:"20"},
-    {type:"h", c: [
-      {type:"btn", font:"25%", label:"0", cb: l=>incScore(layout.server, layout.receiver), id:"server", pad:"10"},
-      {type:"btn", font:"25%", label:"0", cb: l=>incScore(layout.receiver, layout.server), id:"receiver", pad:"10"}
-    ], filly:"1"}
-  ]
-});
+
+function draw(layout) {
+    layout.update();
+    g.clear();
+    layout.render();
+}
+
+function drawStartScreen() {
+    var layout = new Layout( {
+        type:"v", c: [
+            {type:"txt", font: "6x8:2", label:"First server?", pad:"20"},
+            {type:"btn", label:"Player 1", cb: l=>startGame(0), pad:"10"},
+            {type:"btn", label:"Player 2", cb: l=>startGame(1), pad:"10"}
+        ]
+    });
+    draw(layout);
+}
+
+function drawScoreScreen() { 
+  var layout = new Layout( {
+    type:"v", c: [
+      {type:"txt", font:"20%", label:p1GamesWon.toString() + "-" + p2GamesWon.toString(), id:"setScore", pad:"20"},
+      {type:"h", c: [
+        {type:"btn", font:"25%", label:serverScore, cb: l=>incScore(layout.server, layout.receiver), id:"server", pad:"10"},
+        {type:"btn", font:"25%", label:receiverScore, cb: l=>incScore(layout.receiver, layout.server), id:"receiver", pad:"10"}
+      ], filly:"1"}
+    ]
+  });
+  draw(layout);
+}
+
+function startGame(firstServerInput) {
+    firstServer = firstServerInput;
+    drawScoreScreen();
+}
 
 function incScore(scorer, otherPlayer) {
   var newScore;
   var gameEnd = false;
+  var otherPlayerScore = otherPlayer.label;
   switch(scorer.label) {
     case "0":
       newScore = "15";
@@ -26,10 +58,10 @@ function incScore(scorer, otherPlayer) {
       newScore = "40";
       break;
     case "40":
-      if (otherPlayer.label == "Ad") {
+      if (otherPlayerScore == "Ad") {
         newScore = "40";
-        otherPlayer.label = "40";
-      } else if (otherPlayer.label != "40") {
+        otherPlayerScore = "40";
+      } else if (otherPlayerScore != "40") {
         gameEnd = true;
       } else {
         newScore = "Ad";
@@ -42,10 +74,14 @@ function incScore(scorer, otherPlayer) {
   // If the game is still going just update the score,
   // else call a function to reset the game score and update the set score
   if (!gameEnd) {
-    scorer.label = newScore;
-    layout.update();
-    g.clear();
-    layout.render();
+    if (scorer.id == "server") {
+        serverScore = newScore; 
+        receiverScore = otherPlayerScore;
+    } else {
+        receiverScore = newScore;
+        serverScore = otherPlayerScore;
+    }
+    drawScoreScreen();
   } else {
     endGame(scorer);
   }
@@ -53,19 +89,19 @@ function incScore(scorer, otherPlayer) {
 
 function endGame(winner) {
   if (winner.id == "server") {
-    p1GamesWon += 1;
+    p1GamesWon += ((1 + firstServer) % 2);
+    p2GamesWon += firstServer;
   } else {
-    p2GamesWon += 1;
+    p2GamesWon += ((1 + firstServer) % 2);
+    p1GamesWon += firstServer;
   }
   if (isSetWon()) {
     Terminal.println("Set won!");
   } else {
-    layout.server.label = "0";
-    layout.receiver.label = "0";
-    layout.setScore.label = p1GamesWon.toString() + "-" + p2GamesWon.toString();
-    layout.update();
-    g.clear();
-    layout.render();
+    firstServer = (firstServer + 1) % 2;
+    serverScore = "0";
+    receiverScore = "0";
+    drawScoreScreen();
   }
 }
 
@@ -73,5 +109,4 @@ function isSetWon() {
   return (p1GamesWon == 6 && p2GamesWon < 5) || (p2GamesWon == 6 && p1GamesWon < 5) || p1GamesWon == 7 || p2GamesWon == 7;
 }
 
-g.clear();
-layout.render();
+drawStartScreen();
